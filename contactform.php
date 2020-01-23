cf03e6c5148d71b1aa8fc08657f6780ccbb121f8
Erusiafe Joshua <joshuaerusiafe@gmail.com>
2020-01-23 01:58:15 +0100

    now

---
+++ contactform.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['submit'])) {
+$firstname = $_POST['first name'];
+$lastname = $_POST['last name'];
+$subject = $_POST['subject'];
+$mailFrom = $_POST['mail'];
+$message = $_POST['message'];
+
+$mailTo = "joshuaerusiafe@yahoo.com";
+$headers = "From: ".$mailFrom;
+$txt ="You have recieved an email from ".$firstname..$lastname.".\n\n".$message;
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
+}
+?>
+
+
